c3689830b7fb4e40cf79301b408b6def51ddaa2b
김영주 <gim-yeongju@gim-yeongjuui-MacBookPro.local>
2022-03-16 22:28:58 +0900

    Initial commit

---
+++ myautomobileprojectkyj/src/test/java/com/example/myautomobileprojectkyj/MyautomobileprojectkyjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myautomobileprojectkyj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyautomobileprojectkyjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
