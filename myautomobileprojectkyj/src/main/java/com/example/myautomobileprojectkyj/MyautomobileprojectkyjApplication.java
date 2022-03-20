76c2d661ec6a7646884f5c3dd1549cc07f280f2f
gim-yeongju <xenexes@naver.com>
2022-03-21 00:48:07 +0900

    회원가입- 테이블에 최초로 값 입력 성공-메인화면으로 redirect까지

--- myautomobileprojectkyj/src/main/java/com/example/myautomobileprojectkyj/MyautomobileprojectkyjApplication.java
+++ myautomobileprojectkyj/src/main/java/com/example/myautomobileprojectkyj/MyautomobileprojectkyjApplication.java
@@ -1,9 +1,11 @@
 package com.example.myautomobileprojectkyj;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.myautomobileprojectkyj.mapper")
 public class MyautomobileprojectkyjApplication {
 
     public static void main(String[] args) {
