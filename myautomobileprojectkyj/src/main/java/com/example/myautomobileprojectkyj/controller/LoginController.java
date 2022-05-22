53148a7b89c71b3d234db1cdf2cac4e06b686272
gim-yeongju <xenexes@naver.com>
2022-05-22 21:26:45 +0900

    2022.4.18_2 버전 이메일의 도메인부분 db에 입력되지 않는 문제 수정 && 세션상 로그인 로그아웃 기능 구현

--- myautomobileprojectkyj/src/main/java/com/example/myautomobileprojectkyj/controller/LoginController.java
+++ myautomobileprojectkyj/src/main/java/com/example/myautomobileprojectkyj/controller/LoginController.java
@@ -2,11 +2,14 @@ package com.example.myautomobileprojectkyj.controller;
 
 import com.example.myautomobileprojectkyj.service.MemberService;
 import com.example.myautomobileprojectkyj.vo.MemberVo;
+import java.util.logging.*;
+import org.apache.ibatis.logging.Log;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Locale;
@@ -18,15 +21,29 @@ public class LoginController {
 
     private final MemberService memberService;
 
+    private final static Logger LOG = Logger.getGlobal();
+
     public LoginController(MemberService memberService) {
         this.memberService = memberService;
     }
 
     @RequestMapping("/loginpage")
-    public ModelAndView loginController(ModelAndView ModelAndView){
+    public ModelAndView loginController(ModelAndView ModelAndView, HttpServletRequest request){
     System.out.println("loginController 실행됨 : loginpage" );
     ModelAndView mv = new ModelAndView();
-    mv.addObject("loginResult","3");
+
+    HttpSession session = request.getSession(false);
+
+                          session.getId();
+
+    System.out.println("loginpage controller 실행");
+    if(session != null){
+        String id = (String) session.getAttribute("id");
+        mv.addObject("id", id);
+        mv.addObject("loginParam", "Y");
+        System.out.println("로그인 세션 발견");
+    }
+
     mv.setViewName("login/loginpage");
 
     return mv;
@@ -38,27 +55,12 @@ public class LoginController {
         ModelAndView mv = new ModelAndView();
         mv.setViewName("login/joinpage");
 
-
-        String inputYn = request.getParameter("inputYn");
-        String zipNo = request.getParameter("zipNo");
-        String roadAddrPart1 = request.getParameter("roadAddrPart1");
-        String roadAddrPart2 = request.getParameter("roadAddrPart2");
-        String addrDetail = request.getParameter("addrDetail");
-        String jibunAddr = request.getParameter("jibunAddr");
-
-        mv.addObject("inputYn", inputYn);
-        mv.addObject("zipNo", zipNo);
-        mv.addObject("roadAddrPart1", roadAddrPart1);
-        mv.addObject("roadAddrPart2", roadAddrPart2);
-        mv.addObject("jibunAddr", jibunAddr);
-        mv.addObject("addrDetail", addrDetail);
-
         return mv;
     }
 
     @RequestMapping("/joinprocess")
     public ModelAndView joinController( ModelAndView ModelAndView, MemberVo memberVo) throws Exception {
-        System.out.println("loginController 실행됨 : joinprocess" );
+
         ModelAndView mv = new ModelAndView();
 
         memberService.joinMember(memberVo);
@@ -68,12 +70,14 @@ public class LoginController {
     }
 
     @RequestMapping("/loginprocess")
-    public ModelAndView loginProcessController(ModelAndView ModelAndView, HttpServletRequest request, String id, String password) throws Exception {
-        System.out.println("loginController 실행됨 : loginProcess" );
+    public ModelAndView loginProcessController(ModelAndView ModelAndView,HttpServletRequest request, HttpServletResponse response, String id, String password) throws Exception {
+
         ModelAndView mv = new ModelAndView();
         MemberVo memberVo = new MemberVo();
 
         int countOfMember = 0;
+
+        System.out.println("loginprocess 싫행");
 
         if(id != null && password != null){
             memberVo.setId(id);
@@ -84,12 +88,26 @@ public class LoginController {
             }
         }
 
+
+
         if(countOfMember > 0 ){
+
+            HttpSession session = request.getSession();
+
+            session.setMaxInactiveInterval(1800);
+
+            session.setAttribute("id", memberVo.getId());
+            session.setAttribute("name", memberVo.getName());
+            session.setAttribute("email", memberVo.getEmail());
+            session.setAttribute("address", memberVo.getAddress());
+            session.setAttribute("address2", memberVo.getAddress2());
+
             mv.addObject("loginResult","1");
             mv.setViewName("index");
-            HttpSession session = request.getSession();
             //세션에 로그인 회원 정보 보관
             session.setAttribute("loginSession",memberVo.getId() );
+            System.out.println("memberVo.getId() ===="+ memberVo.getId() );
+            mv.addObject("id" ,memberVo.getId());
             System.out.println("로그인 성공");
         }else{
             mv.addObject("loginResult","2");
@@ -100,17 +118,25 @@ public class LoginController {
         return mv;
     }
 
-    @RequestMapping("/logoutprocess")
-    public ModelAndView logoutController(ModelAndView ModelAndView, HttpServletRequest request) throws Exception {
-
-        HttpSession session = request.getSession(false);
-        ModelAndView mv = new ModelAndView();
-        if (session != null) {
-            session.invalidate();
-            mv.addObject("logout","1");
+    @RequestMapping(value = "/logoutprocess.do", method=RequestMethod.POST)
+    @ResponseBody
+    public HashMap<String,String> logoutController(ModelAndView ModelAndView, HttpServletRequest request) throws Exception {
+        HashMap<String,String> map = new HashMap<>();
+        HttpSession session = request.getSession();
+        String id =(String) session.getAttribute("loginSession");
+        System.out.println("logoutprocess.do");
+        System.out.println("id ====" + id);
+        if (id != null) {
+            map.put("loginParam","N");
+            System.out.println("로그아웃 프로세스 실행");
+            session.removeAttribute("loginSession");
+            LOG.info("id = "+ session.getAttribute("loginSession"));
+        }else{
+            map.put("loginParam","Y");
+            System.out.println("로그아웃 프로세스 실행실패");
         }
-        mv.setViewName("index");
-        return mv;
+
+        return map;
     }
 
 
